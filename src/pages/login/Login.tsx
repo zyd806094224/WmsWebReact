e9223ca38e90d80b9d6096e4777921cf96c5580f
zhaoyudong <806094224@qq.com>
2024-03-16 21:05:57 +0800

    登录页面样式调整

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import {Button, Checkbox, Form, FormProps, Input} from "antd";
+import '../login/login.css'
+
+
+type FieldType = {
+    username?: string;
+    password?: string;
+};
+
+
+const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log('Success:', values);
+};
+
+const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+};
+
+export default class Login extends React.Component {
+
+    render() {
+        return (
+            <div className={"loginBody"}>
+                <div className={"loginDiv"}>
+                    <div className="login-content">
+                        <h1 className={"login-title"}>用户登录</h1>
+                        <Form
+                            name="basic"
+                            labelCol={{span: 8}}
+                            wrapperCol={{span: 12}}
+                            style={{maxWidth: 600}}
+                            initialValues={{remember: true}}
+                            onFinish={onFinish}
+                            onFinishFailed={onFinishFailed}
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                label="账号"
+                                name="username"
+                                rules={[{required: true, message: '请输入账号'}]}
+                            >
+                                <Input/>
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                label="密码"
+                                name="password"
+                                rules={[{required: true, message: '请输入密码'}]}
+                            >
+                                <Input.Password/>
+                            </Form.Item>
+
+                            <Form.Item wrapperCol={{offset: 8, span: 16}}>
+                                <Button type="primary" htmlType="submit">
+                                    确定
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Routes, Route, Router, HashRouter} from 'react-router-dom'
 import Home from "../pages/Home";
-import Login from "../pages/Login";
+import Login from "../pages/login/Login";
 import {Index} from "../pages/Index";
 
 class AppRouter extends React.Component {
